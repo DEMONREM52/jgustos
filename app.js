cb7b506c31c19a9a771bb88ecff32d9a95675c3b
DEMONREM52 <javiergarciamontoya2003@gmail.com>
2024-05-15 00:17:12 -0500

    second commit

--- app.js
+++ app.js
@@ -10,9 +10,9 @@ const { render } = require("ejs");
 // Configuración de Express
 const app = express();
 const port = process.env.PORT || 3000;
-const DB_HOST = process.env.DB_HOST || "localhost";
-const DB_USER = process.env.DB_USER || "root";
-const DB_PASSWORD = process.env.DB_PASSWORD || "";
+const DB_HOST = process.env.DB_HOST || "gustos_db";
+const DB_USER = process.env.DB_USER || "mysql";
+const DB_PASSWORD = process.env.DB_PASSWORD || "Javier28670056";
 const DB_NAME = process.env.DB_NAME || "gustos";
 const DB_PORT = process.env.DB_PORT || "3306";
 
