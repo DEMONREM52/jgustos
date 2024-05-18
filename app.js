91aaa66405a776e76050d9b9c287dbfb6a3bce61
DEMONREM52 <javiergarciamontoya2003@gmail.com>
2024-05-17 22:50:04 -0500

    Cambio de diseño en index

--- app.js
+++ app.js
@@ -9,12 +9,20 @@ const bcrypt = require("bcrypt");
 const { render } = require("ejs");
 // Configuración de Express
 const app = express();
+// const port = process.env.PORT || 3000;
+// const DB_HOST = process.env.DB_HOST || "localhost";
+// const DB_USER = process.env.DB_USER || "root";
+// const DB_PASSWORD = process.env.DB_PASSWORD || "";
+// const DB_NAME = process.env.DB_NAME || "gustos";
+// const DB_PORT = process.env.DB_PORT || "3306";
+
 const port = process.env.PORT || 80;
 const DB_HOST = process.env.DB_HOST || "gustos_dbgustos";
 const DB_USER = process.env.DB_USER || "mysql";
 const DB_PASSWORD = process.env.DB_PASSWORD || "6960bde9b53598e7a62c";
 const DB_NAME = process.env.DB_NAME || "gustos";
 const DB_PORT = process.env.DB_PORT || "3306";
+
 
 app.use(
   session({
