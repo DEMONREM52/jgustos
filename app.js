22a6ba7059bc1ab2629633acf521d0d9b8e18843
DEMONREM52 <javiergarciamontoya2003@gmail.com>
2024-05-19 23:57:23 -0500

    Cambios de interfaz de agregue SweetAlert

--- app.js
+++ app.js
@@ -16,19 +16,19 @@ const app = express();
 
 
 
-const port = process.env.PORT || 8000;
-const DB_HOST = process.env.DB_HOST || "localhost";
-const DB_USER = process.env.DB_USER || "root";
-const DB_PASSWORD = process.env.DB_PASSWORD || "";
-const DB_NAME = process.env.DB_NAME || "gustos";
-const DB_PORT = process.env.DB_PORT || "3306";
-
-// const port = process.env.PORT || 80;
-// const DB_HOST = process.env.DB_HOST || "gustos_dbgustos";
-// const DB_USER = process.env.DB_USER || "mysql";
-// const DB_PASSWORD = process.env.DB_PASSWORD || "6960bde9b53598e7a62c";
+// const port = process.env.PORT || 8000;
+// const DB_HOST = process.env.DB_HOST || "localhost";
+// const DB_USER = process.env.DB_USER || "root";
+// const DB_PASSWORD = process.env.DB_PASSWORD || "";
 // const DB_NAME = process.env.DB_NAME || "gustos";
 // const DB_PORT = process.env.DB_PORT || "3306";
+
+const port = process.env.PORT || 80;
+const DB_HOST = process.env.DB_HOST || "gustos_dbgustos";
+const DB_USER = process.env.DB_USER || "mysql";
+const DB_PASSWORD = process.env.DB_PASSWORD || "6960bde9b53598e7a62c";
+const DB_NAME = process.env.DB_NAME || "gustos";
+const DB_PORT = process.env.DB_PORT || "3306";
 
 
 
